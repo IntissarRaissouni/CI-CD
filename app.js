e0098f7ec339a8d7bc4df765b0c2822b283d557b
IntissarRaissouni <intissar.itri@gmail.com>
2025-03-26 16:55:35 +0000

    virtu

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Bonjour depuis Docker !');
+});
+app.listen(3000, () => {
+  console.log('Serveur démarré sur le port 3000');
+});
